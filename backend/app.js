387dbb1c1e11f815e6b291600180a8d843e92b58
deepcu-it <deepghosal445@gmail.com>
2024-08-13 23:24:30 +0530

    added cors

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import errorhandler from "./middleware/error.js";
-
+import cors from "cors";
 const app=express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 import product from "./routes/productRoute.js";
 import user from "./routes/userRoute.js";
