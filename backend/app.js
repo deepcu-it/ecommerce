9396fb7eeae8bdb6e452ffa8e8caaca45f6127e3
deepcu-it <deepghosal445@gmail.com>
2024-08-14 12:19:58 +0530

    Directory structure changed

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,11 @@ import cookieParser from "cookie-parser";
 import errorhandler from "./middleware/error.js";
 import cors from "cors";
 const app=express();
+app.use(cors());
+
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 
 import product from "./routes/productRoute.js";
 import user from "./routes/userRoute.js";
